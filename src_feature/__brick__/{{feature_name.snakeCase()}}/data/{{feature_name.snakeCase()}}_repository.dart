e59380e187549a0f856c929d6ef8a63aa94bbd16
Yama Roni <20947540+yamaroni@users.noreply.github.com>
2026-01-30 11:28:08 +0700

    feat: add feature choice for legacy or abstract

--- src_feature/__brick__/{{feature_name.snakeCase()}}/data/{{feature_name.snakeCase()}}_repository.dart
+++ src_feature/__brick__/{{feature_name.snakeCase()}}/data/{{feature_name.snakeCase()}}_repository.dart
@@ -1,3 +1,6 @@
-abstract class {{feature_name.pascalCase()}}Repository {
+{{^use_legacy}}abstract class {{feature_name.pascalCase()}}Repository {
   // TODO: implement repository
-}
+}{{/use_legacy}}
+{{#use_legacy}}
+// TODO: Add your provider here
+{{/use_legacy}}
--- src_feature/__brick__/{{feature_name.snakeCase()}}/presentation/controller/{{feature_name.snakeCase()}}_controller.dart
+++ src_feature/__brick__/{{feature_name.snakeCase()}}/presentation/controller/{{feature_name.snakeCase()}}_controller.dart
@@ -1,8 +1,8 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+{{^use_legacy}}import 'package:flutter_riverpod/flutter_riverpod.dart';{{/use_legacy}}
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../data/{{feature_name.snakeCase()}}_repository.dart';
-import '../../data/remote/{{feature_name.snakeCase()}}_repo_impl.dart';
+{{^use_legacy}}import '../../data/remote/{{feature_name.snakeCase()}}_repo_impl.dart';{{/use_legacy}}
 
 part '{{feature_name.snakeCase()}}_controller.g.dart';
 
@@ -14,7 +14,7 @@ class {{feature_name.pascalCase()}}Controller extends _${{feature_name.pascalCas
   // TODO: implement controller
 }
 
-/// Interface for {{feature_name.pascalCase()}}Repository
+{{^use_legacy}}/// Interface for {{feature_name.pascalCase()}}Repository
 final {{feature_name.camelCase()}}RepoProvider = Provider<{{feature_name.pascalCase()}}Repository>((ref) {
   return Remote{{feature_name.pascalCase()}}RepositoryImpl(ref);
-});
+});{{/use_legacy}}
